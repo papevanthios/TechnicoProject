782bd57e9aef5b8c11491f809589fef857a2bbdc
LIsrayelyan <amaetp@gmail.com>
2022-03-18 11:04:21 +0200

    update localhost/home

--- src/main/java/gr/codehub/accenture/technicoproject/controller/TechnicoController.java
+++ src/main/java/gr/codehub/accenture/technicoproject/controller/TechnicoController.java
@@ -1,16 +1,22 @@
 package gr.codehub.accenture.technicoproject.controller;
 
+import gr.codehub.accenture.technicoproject.model.PropertyOwner;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @AllArgsConstructor
 public class TechnicoController {
+//    private Object Owner;
     // attributes for services
 
     // endpoint home maria
-
+//
     @GetMapping(value="/home")
     public String home(){
         return "home page";
@@ -19,8 +25,19 @@ public class TechnicoController {
 
     // property owner create maria
 
+//    @PostMapping(value="/owner")
+//    public PropertyOwner create(@RequestBody PropertyOwner owner)
+//    {
+//        return PropertyOwner.createPropertyOwner(Owner);
+//    }
+
     // property owner search1 maria
 
+//    @SearchMapping(value = "/Date")
+//    public Date search(@RequestBody Date date)
+//    {
+//        int Date = 0;
+//        return Date.(Date);
     // property owner search2 maria
 
     // property owner update aris
