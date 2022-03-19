4cea964b18fa247830e783d34bf2561ba239a214
Evanthios Papadopoulos <papevanthios@gmail.com>
2022-03-20 00:24:31 +0200

    Added methods to interface PropertyRepairOrderService.

--- src/main/java/gr/codehub/accenture/technicoproject/service/PropertyRepairOrderService.java
+++ src/main/java/gr/codehub/accenture/technicoproject/service/PropertyRepairOrderService.java
@@ -1,17 +1,25 @@
 package gr.codehub.accenture.technicoproject.service;
 
+import gr.codehub.accenture.technicoproject.exception.PropertyRepairOrderException;
+import gr.codehub.accenture.technicoproject.model.PropertyRepairOrder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PropertyRepairOrderService {
 
-    // methods()
-
-    // property repair create kostas
+    // property repair create manthos
+    PropertyRepairOrder createPropertyRepairOrder(PropertyRepairOrder propertyRepairOrder, int propertyOwnerId) throws PropertyRepairOrderException;
 
     // property repair search1 manthos
-    // This is mine only
+    List<PropertyRepairOrder> searchByPropertyOwnerIdForPropertyRepairOrder(int propertyOwnerId) throws PropertyRepairOrderException;
 
-    // property repair search2
+    // property repair search2 manthos
+    List<PropertyRepairOrder> searchByRangeOfDates(LocalDateTime firstDate, LocalDateTime secondDate);
 
-    // property repair update
+    // property repair update manthos
+    PropertyRepairOrder updatePropertyRepairOrder(int propertyRepairOrderId, PropertyRepairOrder propertyRepairOrder) throws PropertyRepairOrderException;
 
-    // property repair delete
+    // property repair delete manthos
+    boolean deletePropertyRepairOrder(int propertyRepairOrderId) throws PropertyRepairOrderException;
 }
