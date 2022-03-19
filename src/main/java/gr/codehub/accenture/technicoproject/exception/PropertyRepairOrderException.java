3b063bddfcab752ab2913428cf86733cbbaf02c4
Evanthios Papadopoulos <papevanthios@gmail.com>
2022-03-20 00:20:30 +0200

    Added PropertyRepairOrderException file.

---
+++ src/main/java/gr/codehub/accenture/technicoproject/exception/PropertyRepairOrderException.java
@@ -0,0 +1,7 @@
+package gr.codehub.accenture.technicoproject.exception;
+
+public class PropertyRepairOrderException extends Exception{
+    public PropertyRepairOrderException(String message) {
+        super(message);
+    }
+}
