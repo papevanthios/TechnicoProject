79ca977342d4fb38475edeb265b5332e2e8018d4
Evanthios Papadopoulos <papevanthios@gmail.com>
2022-03-22 22:43:31 +0200

    Added javadocs to GeneralController class.

--- src/main/java/gr/codehub/accenture/technicoproject/controller/GeneralController.java
+++ src/main/java/gr/codehub/accenture/technicoproject/controller/GeneralController.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The general controller contains only the home endpoint of the project.
+ */
 @RestController
 @AllArgsConstructor
 public class GeneralController {
