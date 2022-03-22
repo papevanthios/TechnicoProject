89bed52fff4632b3762f75d35d9849da83e4776c
aris_giotis <apjoti@gmail.com>
2022-03-22 22:23:22 +0200

    Javadoc to PropertyOwnerServiceImpl & PropertyOwnerService.

--- src/main/java/gr/codehub/accenture/technicoproject/service/PropertyOwnerService.java
+++ src/main/java/gr/codehub/accenture/technicoproject/service/PropertyOwnerService.java
@@ -3,17 +3,58 @@ package gr.codehub.accenture.technicoproject.service;
 import gr.codehub.accenture.technicoproject.dto.ResponseResultDto;
 import gr.codehub.accenture.technicoproject.model.PropertyOwner;
 
+/**
+ * This is the interface for the Services related to the owner.
+ * Here we define the CRUD operations related to the controller of the owner's page.
+ */
 public interface PropertyOwnerService {
 
+    /**
+     * Creates an owner by the user input through the controller.
+     *
+     * @param propertyOwner  the owner, not null
+     * @return the owner data, status of request and a message
+     */
     ResponseResultDto<PropertyOwner> createPropertyOwner(PropertyOwner propertyOwner);
 
+    /**
+     * Obtains the owner by his VAT number.
+     *
+     * @param propertyOwnerVAT  the VAT number of the owner, 9-digits number
+     * @return  the owner data, status of request and a message
+     */
     ResponseResultDto<PropertyOwner> searchByVAT(String propertyOwnerVAT);
 
+    /**
+     * Obtains the owner by his email.
+     *
+     * @param propertyOwnerEmail  the owner's email, []@[].[]
+     * @return  owner's data, status of request and a message
+     */
     ResponseResultDto<PropertyOwner> searchByEmail(String propertyOwnerEmail);
 
+    /**
+     * Obtains the owner by his id primary key in the database.
+     *
+     * @param propertyOwnerId  the owner's id primary key, from 1 to ...
+     * @return  owner's data, status of request and a message
+     */
     ResponseResultDto<PropertyOwner> searchByPropertyOwnerId(int propertyOwnerId);
 
+    /**
+     * Updates the owner's details. Can be one or more at a time.
+     *
+     * @param propertyOwnerId  the owner's id primary key, from 1 to ...
+     * @param propertyOwner  owner's updated data from the controller
+     * @return  updated owner's details, status of request and a message
+     */
     ResponseResultDto<PropertyOwner> updatePropertyOwner(int propertyOwnerId, PropertyOwner propertyOwner);
 
+    /**
+     * Deletes the owner from the database.
+     *
+     * @param propertyOwnerId  owner's id primary key, from 1 to ...
+     * @return  boolean, true in case of deletion otherwise returns false
+     */
     ResponseResultDto<Boolean> deletePropertyOwner(int propertyOwnerId);
 }
--- src/main/java/gr/codehub/accenture/technicoproject/service/PropertyOwnerServiceImpl.java
+++ src/main/java/gr/codehub/accenture/technicoproject/service/PropertyOwnerServiceImpl.java
@@ -11,6 +11,15 @@ import org.springframework.stereotype.Service;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * This class implements the interface for the Service of the property owner.
+ * <p>
+ * The implementations create, search, update or delete a property owner according to the
+ * request received from the REST-API.
+ * </p>
+ *
+ * throws ERROR in case of unhandled exceptions, e.g. error occurred by the database.
+ */
 @Service
 @AllArgsConstructor
 @Slf4j
@@ -19,16 +28,22 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
 
     private static final String LINE_DELIMITER = "---------------------------------------";
 
+    /**
+     * Creates a property owner by the details inserted from the PropertyOwnerController.
+     *
+     * @param propertyOwner  property owner to represent, not null
+     * @return  the property owner saved in the database, not null
+     */
     @Override
     public ResponseResultDto<PropertyOwner> createPropertyOwner(PropertyOwner propertyOwner) {
         log.info("");
         log.info("Creating a property owner...");
         log.info(LINE_DELIMITER);
 
-        try {
+        try {// save the owner in database
             propertyOwnerRepository.save(propertyOwner);
         }
-        catch(Exception e) {
+        catch(Exception e) {// in case of unhandled exception
             log.error("Exception enabled.");
             log.info(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.ERROR, "An error occurred.");
@@ -38,6 +53,13 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
         return new ResponseResultDto<>(propertyOwner, ResponseStatus.SUCCESS, "The property owner has been created.");
     }
 
+    /**
+     * Obtains the property owner by his VAT number.
+     *
+     * @param propertyOwnerVAT  vat number of the property's owner to represent, exactly 9 digits
+     * @return  the owner, not null
+     * throws ERROR if owner cannot be found
+     */
     @Override
     public ResponseResultDto<PropertyOwner> searchByVAT(String propertyOwnerVAT){
         log.info("");
@@ -45,17 +67,17 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
         log.info(LINE_DELIMITER);
 
         PropertyOwner propertyOwner = null;
-        try {
+        try {// looping through the database to find owner with specific vat number
             for (PropertyOwner propertyOwnerRep : propertyOwnerRepository.findAll())
                 if (Objects.equals(propertyOwnerRep.getVatNumber(), propertyOwnerVAT))
                     propertyOwner = propertyOwnerRep;
-        } catch(Exception e) {
+        } catch(Exception e) {// for an unhandled exception
             log.error("Exception enabled.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.ERROR, "An error occurred.");
         }
 
-        if (propertyOwner == null) {
+        if (propertyOwner == null) {// if there is no such owner throw an error
             log.error("Property owner is null.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_NOT_FOUND, "The property owner was not found.");
@@ -65,6 +87,13 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
         return new ResponseResultDto<>(propertyOwner, ResponseStatus.SUCCESS, "The property owner was found.");
     }
 
+    /**
+     * Obtains the property owner by his email.
+     *
+     * @param propertyOwnerEmail the email of the property owner to represent, [...]@[...].[...]
+     * @return  the property owner
+     * throws  ERROR if the owner cannot be found
+     */
     @Override
     public ResponseResultDto<PropertyOwner> searchByEmail(String propertyOwnerEmail) {
         log.info("");
@@ -72,16 +101,16 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
         log.info(LINE_DELIMITER);
 
         PropertyOwner propertyOwner = null;
-        try {
+        try {// looping through database to find owner with corresponding email
             for (PropertyOwner propertyOwnerRep : propertyOwnerRepository.findAll())
                 if (Objects.equals(propertyOwnerRep.getEmail(), propertyOwnerEmail))
                     propertyOwner = propertyOwnerRep;
-        } catch(Exception e) {
+        } catch(Exception e) {// for an unhandled exception
             log.error("Exception enabled.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.ERROR, "An error occurred.");
         }
-        if (propertyOwner == null) {
+        if (propertyOwner == null) {// if there is no such owner throw an error
             log.error("Property owner is null.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_NOT_FOUND, "The property owner was not found.");
@@ -91,6 +120,13 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
         return new ResponseResultDto<>(propertyOwner, ResponseStatus.SUCCESS, "The property owner was found.");
     }
 
+    /**
+     * Obtains the property owner by his unique id.
+     *
+     * @param propertyOwnerId  id of the owner to represent, from 1 to ...
+     * @return  the property owner
+     * throws ERROR if the owner cannot be found
+     */
     @Override
     public ResponseResultDto<PropertyOwner> searchByPropertyOwnerId(int propertyOwnerId) {
         log.info("");
@@ -98,18 +134,18 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
         log.info(LINE_DELIMITER);
 
         PropertyOwner propertyOwner = null;
-        try {
+        try {// looping through database to find owner with corresponding id number
             for (PropertyOwner propertyOwnerRep : propertyOwnerRepository.findAll())
                 if (Objects.equals(propertyOwnerRep.getPropertyOwnerId(), propertyOwnerId))
                     propertyOwner = propertyOwnerRep;
         }
-        catch(Exception e) {
+        catch(Exception e) {// for an unhandled exception
             log.error("Exception enabled.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.ERROR, "An error occurred.");
         }
 
-        if (propertyOwner == null) {
+        if (propertyOwner == null) {// if no such owner exists throw an error
             log.error("Property owner was not found.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_NOT_FOUND, "The property owner was not found.");
@@ -119,12 +155,30 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
         return new ResponseResultDto<>(propertyOwner, ResponseStatus.SUCCESS, "The property owner was found.");
     }
 
+    /**
+     * Update the details of the property owner.
+     * <p><ul>
+     * <li>VAT number
+     * <li>phone number
+     * <li>username
+     * <li>password
+     * <li>email
+     * <li>address
+     * <li>first name
+     * <li>last name
+     * </ul></p>
+     *
+     * @param propertyOwnerId the id of the property owner, from 1 to ...
+     * @param propertyOwner the property owner with the desired details to update
+     * @return  the property owner with the updated details
+     * throws ERROR for an unhandled exception or if no updates were found
+     */
     @Override
     public ResponseResultDto<PropertyOwner> updatePropertyOwner(int propertyOwnerId, PropertyOwner propertyOwner) {
         log.info("");
         log.info("Updating property owner's details...");
         log.info(LINE_DELIMITER);
-        // Check if property owner is null
+        // check if all details of the owner are null, if yes throw an error
         if (    propertyOwner.getVatNumber() == null &&
                 propertyOwner.getPhoneNumber() == null &&
                 propertyOwner.getPassword() == null &&
@@ -141,106 +195,106 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
         // If property owner is not null proceed
         Optional<PropertyOwner> propertyOwnerOpt;
 
-        try {
+        try {// find owner from database through the primary key
             propertyOwnerOpt = propertyOwnerRepository.findById(propertyOwnerId);
         }
-        catch (Exception e){
+        catch (Exception e){// for an unhandled exception
             log.error("Exception enabled.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.ERROR, "An error occurred.");
         }
-        // Check if property owner is empty
+        // in case there is no owner found from the primary key, throw a message
         if (propertyOwnerOpt.isEmpty()) {
             log.error("Property owner was not found.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_NOT_FOUND, "The property owner was not found.");
         }
 
-        // Check every possible field for user input, and update it.
+        // Check every possible field for user input, and update it. If a field is inserted then set it.
         try {
-            if (propertyOwner.getVatNumber() != null)
+            if (propertyOwner.getVatNumber() != null)// vat number
                 propertyOwnerOpt.get().setVatNumber(propertyOwner.getVatNumber());
         }
-        catch (Exception e) {
+        catch (Exception e) {// if value cannot be set throw an error message
             log.error("VAT number is incorrect.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_INFORMATION_ARE_INCORRECT, "The VAT number is incorrect.");
         }
 
         try {
-            if (propertyOwner.getFirstName() != null)
+            if (propertyOwner.getFirstName() != null)// first name
                 propertyOwnerOpt.get().setFirstName(propertyOwner.getFirstName());
         }
-        catch (Exception e) {
+        catch (Exception e) {// if value cannot be set throw an error message
             log.error("First name is incorrect.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_INFORMATION_ARE_INCORRECT, "The first name is incorrect.");
         }
 
         try {
-            if (propertyOwner.getLastName() != null)
+            if (propertyOwner.getLastName() != null)// last name
                 propertyOwnerOpt.get().setLastName(propertyOwner.getLastName());
         }
-        catch (Exception e) {
+        catch (Exception e) {// if value cannot be set throw an error message
             log.error("Last name is incorrect.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_INFORMATION_ARE_INCORRECT, "The last name is incorrect.");
         }
 
         try {
-            if (propertyOwner.getAddress() != null)
+            if (propertyOwner.getAddress() != null)// address
                 propertyOwnerOpt.get().setAddress(propertyOwner.getAddress());
         }
-        catch (Exception e) {
+        catch (Exception e) {// if value cannot be set throw an error message
             log.error("Address is incorrect.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_INFORMATION_ARE_INCORRECT, "The address is incorrect.");
         }
 
         try {
-            if (propertyOwner.getPhoneNumber() != null)
+            if (propertyOwner.getPhoneNumber() != null)// phone number
                 propertyOwnerOpt.get().setPhoneNumber(propertyOwner.getPhoneNumber());
         }
-        catch (Exception e) {
+        catch (Exception e) {// if value cannot be set throw an error message
             log.error("Phone number is incorrect.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_INFORMATION_ARE_INCORRECT, "The phone number is incorrect.");
         }
 
         try {
-            if (propertyOwner.getEmail() != null)
+            if (propertyOwner.getEmail() != null)// email
                 propertyOwnerOpt.get().setEmail(propertyOwner.getEmail());
         }
-        catch (Exception e) {
+        catch (Exception e) {// if value cannot be set throw an error message
             log.error("Email is incorrect.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_INFORMATION_ARE_INCORRECT, "The email is incorrect.");
         }
 
         try {
-            if (propertyOwner.getUsername() != null)
+            if (propertyOwner.getUsername() != null)// username
                 propertyOwnerOpt.get().setUsername(propertyOwner.getUsername());
         }
-        catch (Exception e) {
+        catch (Exception e) {// if value cannot be set throw an error message
             log.error("Username is incorrect.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_INFORMATION_ARE_INCORRECT, "The username is incorrect.");
         }
 
         try {
-            if (propertyOwner.getPassword() != null)
+            if (propertyOwner.getPassword() != null)// password
                 propertyOwnerOpt.get().setPassword(propertyOwner.getPassword());
         }
-        catch (Exception e) {
+        catch (Exception e) {// if value cannot be set throw an error message
             log.error("Password is incorrect.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(null, ResponseStatus.PROPERTY_OWNER_INFORMATION_ARE_INCORRECT, "The password is incorrect.");
         }
 
         try {
-            propertyOwnerRepository.save(propertyOwnerOpt.get());
+            propertyOwnerRepository.save(propertyOwnerOpt.get());// if input is valid then save to database
         }
-        catch (Exception e) {
+        catch (Exception e) {// for an unhandled exception throw an error message
             log.error("Exception enabled.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(propertyOwnerOpt.get(), ResponseStatus.ERROR, "An error occurred.");
@@ -250,6 +304,13 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
         return new ResponseResultDto<>(propertyOwnerOpt.get(), ResponseStatus.SUCCESS, "The property owner's details have been updated.");
     }
 
+    /**
+     * Deletes the property owner.
+     *
+     * @param propertyOwnerId  id of the property owner, from 1 to ...
+     * @return boolean, true for successfull deletion
+     * throws ERROR for an unhandled exception or if the owner was not found
+     */
     @Override
     public ResponseResultDto<Boolean> deletePropertyOwner(int propertyOwnerId) {
         log.info("");
@@ -261,32 +322,32 @@ public class PropertyOwnerServiceImpl implements PropertyOwnerService {
         try {
             propertyOwnerOpt = propertyOwnerRepository.findById(propertyOwnerId);
         }
-        catch(Exception e) {
+        catch(Exception e) {// for an unhandled exception throw an error message
             log.error("ERROR occurred.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(false, ResponseStatus.ERROR, "An error occurred.");
         }
 
-        if (propertyOwnerOpt.isEmpty()) {
+        if (propertyOwnerOpt.isEmpty()) {// if no owner was found by the primary key
             log.error("Property owner not found.");
             log.error(LINE_DELIMITER);
             return new ResponseResultDto<>(false, ResponseStatus.PROPERTY_OWNER_NOT_FOUND, "The property owner cannot be found.");
         }
-
+        // check if owner has no properties under his possession
         if (propertyOwnerOpt.get().getPropertyList().isEmpty()) {
             try {
-                propertyOwnerRepository.delete(propertyOwnerOpt.get());
+                propertyOwnerRepository.delete(propertyOwnerOpt.get());// delete owner
                 log.info("Successful deletion.");
                 log.info(LINE_DELIMITER);
                 return new ResponseResultDto<>(true, ResponseStatus.SUCCESS, "The property owner has been deleted.");
             }
-            catch(Exception e){
+            catch(Exception e){// for an unhandled exception
                 log.error("Exception enabled.");
                 log.error(LINE_DELIMITER);
                 return new ResponseResultDto<>(false, ResponseStatus.ERROR, "An error occurred.");
             }
 
-        }
+        }// if owner has properties then he cannot be deleted, throw error message
         log.error("Property owner has properties.");
         log.error(LINE_DELIMITER);
         return new ResponseResultDto<>(false, ResponseStatus.ERROR, "Property owner has properties.");
